6d287f29adb123fd80c54f2fec4d6af412734bbb
Wcederskog <57982815+Wcederskog@users.noreply.github.com>
2020-06-07 14:44:08 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,3 +1,9 @@
-<script>
-document.getElementById("demo").innerHTML = "My First JavaScript";
-</script>
+function changeImage() {
+img = document.getElementById("door")
+img.src = img.src.replace("images/door.png", "images/opendoor.png");
+}
+
+function originaldoor() {
+img = document.getElementById("door")
+img.src = img.src.replace("images/opendoor.png" , "images/door.png");
+}
